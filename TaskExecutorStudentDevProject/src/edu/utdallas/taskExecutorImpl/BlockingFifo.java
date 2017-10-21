b2d7584e10a5b819a229603a8222fb884cfd2c2a
Keith Nguyen <khoanguyen0791@users.noreply.github.com>
2017-10-21 02:13:46 -0500

    Add files via upload

--- TaskExecutorStudentDevProject/src/edu/utdallas/taskExecutorImpl/BlockingFifo.java
+++ TaskExecutorStudentDevProject/src/edu/utdallas/taskExecutorImpl/BlockingFifo.java
@@ -6,7 +6,7 @@ public class BlockingFifo {
 	// Use array (requirement 4)
 	private Task[] boundedBuffer;
 	private int size, nextIn, nextOut, count;
-	static Object notFull, notEmpty;
+	Object notFull, notEmpty;
 
 	BlockingFifo(int s) {
 		// initialize list with 100 tasks
@@ -22,48 +22,48 @@ public class BlockingFifo {
 	}
 
 	boolean isFull() {
-		return count == size;
+		return count >= size;
 	}
 
 	public void put(Task task) {
 
-		synchronized (notFull) {
-			try {
-				while (isFull()) {
-					// Thread.yield();
-					notFull.wait();
+		//This is the parallel part of the program.
+		synchronized (this) {
+			while (isFull()) {
+				try {
+					this.wait();
+				} catch (Exception e) {
+					System.out.println(e);
 				}
-			} catch (Exception e) {
-				System.out.println(e);
 			}
 			boundedBuffer[nextIn] = task;
 			count++;
 			nextIn = (nextIn + 1) % size;
-			//System.out.println("Count after add: " + count);
-		}
-
-		synchronized (this) {
 
 		}
-
+		
+		//Use sleep to test the case that producer is faster or slower than consumer thread.
 		try {
 			Thread.sleep(0);
 		} catch (Exception e) {
 		}
 
-		synchronized (notEmpty) {
-			notEmpty.notify();
+		
+		//Once the add/take task is done, the thread can release the blocking array any time.
+		synchronized (this) {
+			this.notify();
 		}
 
 	}
 
 	public Task take() {
 		Task temp;
-		synchronized (notEmpty) {
 
+		// This is the parallel part
+		synchronized (this) {
 			while (isEmpty()) {
 				try {
-					notEmpty.wait();
+					this.wait();
 				} catch (Exception e) {
 					System.out.println(e);
 				}
@@ -72,18 +72,20 @@ public class BlockingFifo {
 			// boundedBuffer[nextOut] = null;
 			nextOut = (nextOut + 1) % size;
 			count--;
-			//System.out.println("Count after take: " + count);
-		}
-
-		synchronized (this) {
 
 		}
+
+		//Use sleep to test the case that producer is faster or slower than consumer thread.
 		try {
-			Thread.sleep(10);
+			Thread.sleep(40);
 		} catch (Exception e) {
 		}
-		synchronized (notFull) {
-			notFull.notify();
+
+		//Once the add/take task is done, the thread can release the blocking array any time.
+		//Potential issue is: if there are only 2 threads, this can lead to deadlock. 
+//However, since we are working on only 1 object, 
+		synchronized (this) {
+			this.notify();
 		}
 
 		return temp;

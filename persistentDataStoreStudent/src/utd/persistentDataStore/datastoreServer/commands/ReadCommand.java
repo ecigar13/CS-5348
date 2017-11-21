73a423a7dffe794dacd3d2b6088808d23c594674
abhijeet-13 <33694709+abhijeet-13@users.noreply.github.com>
2017-11-21 12:02:35 -0600

    Modified structure
    The existing impl. does NOT throw a ServerException, so modified the code to utilize the readData() method from the FileUtil.java

--- persistentDataStoreStudent/src/utd/persistentDataStore/datastoreServer/commands/ReadCommand.java
+++ persistentDataStoreStudent/src/utd/persistentDataStore/datastoreServer/commands/ReadCommand.java
@@ -8,23 +8,38 @@ import java.lang.StringBuilder;
 
 import utd.persistentDataStore.utils.ServerException;
 import utd.persistentDataStore.utils.StreamUtil;
+import utd.persistentDataStore.utils.FileUtil;
 
+/* The read operation will retrieve from the server data previously saved using
+ * the provided name. If the name is not found, the response message will
+ * contain an error message. See the protocol section for a description of the
+ * message structure.
+ */
 public class ReadCommand extends ServerCommand {
-
-	public ReadCommand() {
-		// TODO Auto-generated constructor stub
-		// The read operation will retrieve from the server data previously saved using
-		// the provided name. If the name is not found, the response message will
-		// contain an error message. See the protocol section for a description of the
-		// message structure.
-	}
+	
+	//Initialize a FileUtil class to take care of the read method and throwing appropriate exceptions
+	FileUtil mFileUtil;
 
 	@Override
 	public void run() throws IOException, ServerException {
-		// TODO Auto-generated method stub
+		
+		//Read the name of the record which needs to be read from the inputstream
 		String title = StreamUtil.readLine(inputStream);
 		logger.debug("Reading file: " + title);
+		
+		//read the record in a temporary byte array, an exception will be thrown by readData if the record is not found
+		byte[] tmp = mFileUtil.readData(title);
 
+		sendOK();					//if the read was successful, append an OK to the output stream
+		StreamUtil.writeLine(tmp.length, outputStream);	//append the length of the data corresponding to the record
+		StreamUtil.writeData(tmp, outputStream);	//append the data corresponding to the record
+		
+		//Log the read parameters
+		logger.debug("Bytes: "+ tmp.length);
+		logger.debug("Message: "+ tmp);
+		logger.debug("Finished reading message.\n");
+		
+		/*
 		try {
 			// will throw exception if file doesn't exist
 			FileReader f = new FileReader(title);
@@ -35,8 +50,7 @@ public class ReadCommand extends ServerCommand {
 			byte[] arr = new byte[Integer.valueOf(length)];
 			for(int i = 0;i<arr.length;i++)
 				arr[i]= Byte.valueOf(r.readLine());
-			
-			
+
 			//write the message in specified format
 			this.sendOK();
 			StreamUtil.writeLine(length, outputStream);
@@ -48,15 +62,13 @@ public class ReadCommand extends ServerCommand {
 			logger.debug("Finished reading message.\n");
 			r.close();
 			f.close();
-		}catch(
-
-	Exception e)
-	{
-		String temp = e.toString() + ":" + title + "\n";
-		StreamUtil.writeLine(temp, outputStream);
-		logger.debug(temp);
+		} catch(Exception e)
+		{
+			String temp = e.toString() + ":" + title + "\n";
+			StreamUtil.writeLine(temp, outputStream);
+			logger.debug(temp);
+		}
+		*/
+		
 	}
-
-}
-
 }

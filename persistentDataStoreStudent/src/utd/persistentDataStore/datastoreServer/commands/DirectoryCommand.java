f20427052c362b1df39ba5b45e6da22165bee189
abhijeet-13 <33694709+abhijeet-13@users.noreply.github.com>
2017-11-21 12:41:40 -0600

    Modified structure
    Making use of the FileUtil method

--- persistentDataStoreStudent/src/utd/persistentDataStore/datastoreServer/commands/DirectoryCommand.java
+++ persistentDataStoreStudent/src/utd/persistentDataStore/datastoreServer/commands/DirectoryCommand.java
@@ -7,20 +7,31 @@ import java.io.File;
 import utd.persistentDataStore.utils.ServerException;
 import utd.persistentDataStore.utils.StreamUtil;
 
+/* The directory operation will respond with a list of names that are currently
+ * stored by the service. See the protocol section for a description of the
+ * message structure.
+ */
 public class DirectoryCommand extends ServerCommand {
-
-	public DirectoryCommand() {
-		// TODO Auto-generated constructor stub
-		// /The directory operation will respond with a list of names that are currently
-		// stored by the service. See the protocol section for a description of the
-		// message structure.
-	}
 
 	@Override
 	public void run() throws IOException, ServerException {
-		// directory() implementation
+		
 		logger.debug("Directory request.\n");
-
+		
+		//Obtain the list of all the records
+		List<String> file_list = FileUtil.directory();
+		
+		sendOK();						//Append OK to the output stream if the operation was successful
+		StreamUtil.writeLine(file_list.size(), outputStream);	//Append the number of records to the output stream next
+		//Append names of all the records successively, present in the list
+		for(String s : List){
+			StreamUtil.writeLine(s, outputStream);
+			logger.debug("Listing " + s);
+		}
+		
+		logger.debug("Finished directory request.");
+			
+		/*
 		// get the path
 		String dir = System.getProperty("user.dir");
 		
@@ -37,7 +48,8 @@ public class DirectoryCommand extends ServerCommand {
 		}
 		
 		logger.debug("Finished directory request.");
-
+		*/
+		
 		// End of implementation
 	}
 
